bea6d6640d7949993b0055b394ea9af23758f3de
jherbrandson <jherbrandson@gmail.com>
2023-10-07 16:58:44 -0700

    Update dehumidifier.py
    Changed dehumidifier.py to be a main hub file for the dehumidifier program. Additional updates to follow: changes to file logger.py, and addition of new file relay_control.py

--- dehumidifier.py
+++ dehumidifier.py
@@ -1,66 +1,49 @@
 import Adafruit_DHT
 import time
-import RPi.GPIO as GPIO
+from relay_control import RelayControl
+from logger import Logger
 
-# Set the GPIO pin where the DHT22 sensor is connected
+# Sensor Setup
 DHT_PIN = 23
-RELAY_PIN_1 = 24  # GPIO pin for relay 1 (power supply)
-RELAY_PIN_3 = 27  # GPIO pin for relay 3 (power button)
-RELAY_PIN_4 = 22  # GPIO pin for relay 4 (indicator light)
 SENSOR_TYPE = Adafruit_DHT.DHT22
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(RELAY_PIN_1, GPIO.OUT, initial=GPIO.HIGH)  # assuming relay is off when HIGH
-GPIO.setup(RELAY_PIN_3, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(RELAY_PIN_4, GPIO.OUT, initial=GPIO.HIGH)
-
+# State Variables
 power_turned_on = False
 relay_3_triggered = False
 relay_4_triggered = 0
 
-def activate_relay(pin):
-    GPIO.output(pin, GPIO.LOW)
-    time.sleep(1)
-    GPIO.output(pin, GPIO.HIGH)
+# Create instances of RelayControl and Logger classes
+relay = RelayControl()
+logger = Logger('./logs/enviro.txt')
 
 try:
     while True:
         humidity, temperature_celsius = Adafruit_DHT.read_retry(SENSOR_TYPE, DHT_PIN)
         current_time = time.strftime("%Y-%m-%d %H:%M:%S")
-        print(f"Power Turned On: {power_turned_on}")
-        print(f"Relay 3 Triggered: {relay_3_triggered}")
-        print(f"Relay 4 Triggered: {relay_4_triggered}")
+        log_message = f"{current_time}: Temperature: {temperature_celsius:.2f}°C, Humidity: {humidity:.2f}%"
+        logger.log(log_message)
 
-        if humidity is not None and temperature_celsius is not None:
-            temperature_fahrenheit = (temperature_celsius * 9/5) + 32
-            print(f"{current_time}: Temperature: {temperature_fahrenheit:.2f}°F, Humidity: {humidity:.2f}%")
+        if humidity > 60:
+            if not power_turned_on:
+                relay.activate_relay(relay.RELAY_PIN_1)
+                power_turned_on = True
 
-            if humidity > 60:
-                if not power_turned_on:
-                    activate_relay(RELAY_PIN_1)
-                    print("Dehumidifier is ON")
-                    power_turned_on = True
+            if not relay_3_triggered:
+                relay.activate_relay(relay.RELAY_PIN_3)
+                relay_3_triggered = True
 
-                if not relay_3_triggered:
-                    activate_relay(RELAY_PIN_3)
-                    relay_3_triggered = True
-
-                if relay_3_triggered and relay_4_triggered < 2:
-                    activate_relay(RELAY_PIN_4)
-                    relay_4_triggered += 1
-
-            else:
-                if power_turned_on:
-                    activate_relay(RELAY_PIN_1)
-                    print("Dehumidifier is OFF")
-                    power_turned_on = False
-                    relay_3_triggered = False
-                    relay_4_triggered = 0
+            if relay_3_triggered and relay_4_triggered < 2:
+                relay.activate_relay(relay.RELAY_PIN_4)
+                relay_4_triggered += 1
 
         else:
-            print(f"{current_time}: Failed to retrieve data from the sensor. Check your connections.")
+            if power_turned_on:
+                relay.activate_relay(relay.RELAY_PIN_1)
+                power_turned_on = False
+                relay_3_triggered = False
+                relay_4_triggered = 0
 
-        time.sleep(10)
+        time.sleep(5)
 
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    relay.cleanup()
