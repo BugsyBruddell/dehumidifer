d5f25d9671ed1323aabbd573c812013efb4b79a1
jherbrandson <jherbrandson@gmail.com>
2023-10-07 16:59:47 -0700

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,28 +1,8 @@
-import Adafruit_DHT
-import time
+class Logger:
 
-# Set the GPIO pin where the DHT22 sensor is connected
-DHT_PIN = 23
-SENSOR_TYPE = Adafruit_DHT.DHT22
+    def __init__(self, log_file_path):
+        self.log_file_path = log_file_path
 
-try:
-    while True:
-        humidity, temperature_celsius = Adafruit_DHT.read_retry(SENSOR_TYPE, DHT_PIN)
-        current_time = time.strftime("%Y-%m-%d %H:%M:%S")
-
-        if humidity is not None and temperature_celsius is not None:
-            temperature_fahrenheit = (temperature_celsius * 9/5) + 32
-            data = f"{current_time}: Temperature: {temperature_fahrenheit:.2f}°F, Humidity: {humidity:.2f}%"
-
-            # Append data to the file
-            with open("/home/bripi/Code/logs/enviro.txt", "a") as file:
-                file.write(data + "\n")
-                print(data)  # Print the data to the console
-
-        else:
-            print(f"{current_time}: Failed to retrieve data from the sensor. Check your connections.")
-
-        time.sleep(10)  # Sleep for 10 seconds before the next reading
-
-except KeyboardInterrupt:
-    pass
+    def log(self, message):
+        with open(self.log_file_path, 'a') as log_file:
+            log_file.write(message + '\n')
