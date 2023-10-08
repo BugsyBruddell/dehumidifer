0b0f1de2f2eeca6fc8b60bc95e7a6b308d859afe
jherbrandson <jherbrandson@gmail.com>
2023-10-07 17:22:17 -0700

    Update logger.py
    Added capability to log relay_control.py

--- logger.py
+++ logger.py
@@ -1,8 +1,13 @@
 class Logger:
 
-    def __init__(self, log_file_path):
-        self.log_file_path = log_file_path
+    def __init__(self, enviro_log_path, relay_log_path):
+        self.enviro_log_path = enviro_log_path
+        self.relay_log_path = relay_log_path
 
-    def log(self, message):
-        with open(self.log_file_path, 'a') as log_file:
-            log_file.write(message + '\n')
+    def log_env_data(self, message):
+        with open(self.enviro_log_path, 'a') as enviro_log_file:
+            enviro_log_file.write(message + '\n')
+
+    def log_relay_activity(self, message):
+        with open(self.relay_log_path, 'a') as relay_log_file:
+            relay_log_file.write(message + '\n')
